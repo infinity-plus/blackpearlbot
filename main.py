e88aeb61f5d83b31729b2bd22093a21d939c9447
Harsh Navdhare <harsh.navdhare@proton.me>
2023-01-18 14:29:17 +0530

    Some more logics improved

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import discord
-import discord.ext
-from discord.ext import commands
 import asyncio
 import json
-from apscheduler.schedulers.background import BackgroundScheduler
 import time
+
+import discord
+import discord.ext
+from apscheduler.schedulers.background import BackgroundScheduler
+from discord.ext import commands
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
@@ -85,12 +86,7 @@ async def taskdone(interaction: discord.Interaction):
             await interaction.response.send_message(
                 "**Task amount updated!** :white_check_mark:"
             )
-        elif (
-            juniorDevs not in interaction.user.roles
-            or seniorDevs not in interaction.user.roles
-            or leadDevs not in interaction.user.roles
-            or projectManagers not in interaction.user.roles
-        ):
+        else:
             await interaction.response.send_message(
                 "**You do not have permission to run this command!** :x:"
             )
@@ -130,14 +126,12 @@ async def viewtasks(interaction: discord.Interaction):
             or leadDevs in interaction.user.roles
             or projectManagers in interaction.user.roles
         ):
-            # Initialize the message
-            message_text = ""
-            # Build the message using string formatting
-            for user_id, value in sorted(
-                data.items(), key=lambda x: x[1], reverse=True
-            ):
-                message_text += f"<@{user_id}>**: {value}**\n"
-
+            message_text = "".join(
+                f"<@{user_id}>**: {value}**\n"
+                for user_id, value in sorted(
+                    data.items(), key=lambda x: x[1], reverse=True
+                )
+            )
             # Create the embed
             task_embed = discord.Embed(
                 title="Tasks Completed This Week",
@@ -148,12 +142,7 @@ async def viewtasks(interaction: discord.Interaction):
             # Send the embed
             await interaction.response.send_message(embed=task_embed)
 
-        elif (
-            juniorDevs not in interaction.user.roles
-            or seniorDevs not in interaction.user.roles
-            or leadDevs not in interaction.user.roles
-            or projectManagers not in interaction.user.roles
-        ):
+        else:
             # The message author does not have one of the roles
             # Don't perform the code actions
             await interaction.response.send_message(
@@ -189,16 +178,11 @@ scheduler.start()
 # non-slash command part of code
 @bot.event
 async def on_message(message):
-    args = str(message.content).lower().split(" ")
+    args = str(message.content).lower().split()
     if args[0] == "bruh":
         await message.channel.send("<:bruh_stone:1059119664543825950>")
 
-    elif args[0] == "i'm" and " ".join(args[1:]) == "horny":
-        await message.channel.send(
-            "https://tenor.com/view/vorzek-vorzneck-oglg-og-lol-gang-gif-24901093"
-        )
-
-    elif args[0] == "im" and " ".join(args[1:]) == "horny":
+    elif args[0] in ("i'm", "im", "I am") and " ".join(args[1:]) == "horny":
         await message.channel.send(
             "https://tenor.com/view/vorzek-vorzneck-oglg-og-lol-gang-gif-24901093"
         )
