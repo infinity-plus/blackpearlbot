178745eab515d0a4a61ea159513b73f37227a02e
Q99kek <121639318+Q99kek@users.noreply.github.com>
2023-01-23 23:01:59 +0100

    Create button confirmation system and update UI

--- filters.py
+++ filters.py
@@ -1,7 +1,6 @@
 import discord
 import sqlite3
 from discord.ext import commands
-import asyncio
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
@@ -11,8 +10,10 @@ bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 async def on_ready():
     synced = await bot.tree.sync()
     print(f"Synced {len(synced)} command(s)")
+    # Connect to the SQLite database
     conn = sqlite3.connect('triggers.db')
     c = conn.cursor()
+    # Create the triggers table if it doesn't exist
     c.execute('''CREATE TABLE IF NOT EXISTS triggers (trigger text, response text)''')
     conn.commit()
     conn.close()
@@ -23,12 +24,12 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
+    # Connect to the SQLite database
     conn = sqlite3.connect('triggers.db')
     c = conn.cursor()
     # Retrieve the response for the triggered word
     c.execute(f"SELECT response FROM triggers WHERE trigger='{message.content.strip().lower()}'")
-    result = c.fetchone()
-    if result:
+    if result := c.fetchone():
         await message.channel.send(result[0])
     conn.close()
 
@@ -41,15 +42,14 @@ async def filter_add(interaction: discord.Interaction, trigger: str, response: s
     c = conn.cursor()
     # Check if the trigger word already exists in the triggers table
     c.execute(f"SELECT * FROM triggers WHERE trigger='{trigger}'")
-    result = c.fetchone()
-    if result:
+    if c.fetchone():
         await interaction.response.send_message(
             f"Trigger word ``{trigger}`` already exists, please use a different word.")
     else:
         # Insert the trigger word and response into the triggers table
         c.execute(f"INSERT INTO triggers (trigger, response) VALUES ('{trigger}', '{response}')")
         conn.commit()
-        await interaction.response.send_message(f"Filter added: {trigger} -> {response}")
+        await interaction.response.send_message(f"Filter added: ``{trigger}`` -> {response}")
     conn.close()
 
 
@@ -61,44 +61,56 @@ async def filter_delete(interaction: discord.Interaction, trigger: str):
     c = conn.cursor()
     # Check if the trigger word exists in the triggers table
     c.execute(f"SELECT * FROM triggers WHERE trigger='{trigger}'")
-    result = c.fetchone()
-    if result:
+    if c.fetchone():
         c.execute(f"DELETE FROM triggers WHERE trigger='{trigger}'")
         conn.commit()
-        await interaction.response.send_message(f"Filter deleted successfully!")
+        await interaction.response.send_message("Filter deleted successfully!")
     else:
         await interaction.response.send_message(f"Trigger word ``{trigger}`` does not exist.")
     conn.close()
 
 
-@bot.tree.command()
-async def filter_wipe(interaction: discord.Interaction):
-    """Wipe the filter database."""
-    # Send a message asking the user to confirm the action
-    confirm_message = await interaction.channel.send(
-        "Are you sure you want to delete all filters?\nThis action cannot be undone.")
-    await confirm_message.add_reaction("👍")
-    await confirm_message.add_reaction("👎")
+class Buttons(discord.ui.View):
+    def __init__(self, author):
+        self.author = author
+        super().__init__()
 
-    def check(reaction, user):
-        return user == interaction.user and str(reaction.emoji) in ("👍", "👎")
-
-    try:
-        reaction, user = await bot.wait_for("reaction_add", check=check, timeout=60.0)
-        if str(reaction.emoji) == "👍":
+    @discord.ui.button(label='Yes', style=discord.ButtonStyle.green)
+    async def yes(self, interaction: discord.Interaction, button: discord.ui.View):
+        if interaction.user == self.author:
+            await interaction.response.send_message('Filter database has been successfully wiped!')
+            self.value = 'Yes'
             conn = sqlite3.connect('triggers.db')
             c = conn.cursor()
             c.execute("DELETE FROM triggers")
             conn.commit()
-            await interaction.channel.send("Filter database has been successfully wiped!")
+            self.stop()
         else:
-            await interaction.channel.send("Filter wipe canceled.")
-    except asyncio.TimeoutError:
-        # If the user does not confirm within 60 seconds, send a message and return
-        await confirm_message.edit(content="Filter wipe cancelled, time limit exceeded.")
-        return
+            await interaction.response.send_message(
+                f'{interaction.user.mention} You are not authorized to perform this action.')
 
-           
+    @discord.ui.button(label='No', style=discord.ButtonStyle.red)
+    async def no(self, interaction: discord.Interaction, button: discord.ui.View):
+        if interaction.user == self.author:
+            await interaction.response.send_message('Filter wipe has been canceled.')
+            self.value = 'No'
+            self.stop()
+        else:
+            await interaction.response.send_message(
+                f'{interaction.user.mention} You are not authorized to perform this action.')
+
+
+@bot.tree.command()
+async def filter_wipe(interaction):
+    """Wipe the filter database."""
+    # Send a message asking the user to confirm the action
+    view = Buttons(interaction.user)
+    embed = discord.Embed(title='Are you sure you want to delete all filters?',
+                          description=':warning: This action cannot be undone.',
+                          color=discord.Color.from_rgb(54, 57, 63))
+    await interaction.channel.send(embed=embed, view=view)
+
+
 @bot.tree.command()
 async def filter_list(interaction: discord.Interaction):
     """List all existing filter triggers and responses."""
@@ -112,13 +124,14 @@ async def filter_list(interaction: discord.Interaction):
         await interaction.response.send_message("There are no filters in the database.")
         conn.close()
         return
-    filters = []
-    # Loop through the result set and append the trigger word and response to a list
-    for trigger, response in result:
-        filters.append(f"{trigger} -> {response}")
+    filters = [f"``{trigger}``-> {response}" for trigger, response in result]
     # Join all items in the list into a single string
     filters_string = "\n".join(filters)
-    await interaction.response.send_message(f"All filters:\n{filters_string}")
+    embed = discord.Embed(title='List of all filters:',
+                          description=f'\n{filters_string}',
+                          color=discord.Color.from_rgb(54, 57, 63))
+    await interaction.channel.send(embed=embed)
     conn.close()
+
 
 bot.run(TOKEN)
