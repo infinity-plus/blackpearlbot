ef6c2de0f375a2fc7ad50ded2d1fb6e2ddfa81d2
Harsh Navdhare <harsh.navdhare@proton.me>
2023-01-28 12:59:31 +0530

    Removing in favour of filters plugin

--- plugins/fun.py
+++ plugins/fun.py
@@ -23,25 +23,6 @@ class Fun(commands.Cog):
             "https://i.pinimg.com/originals/f3/25/40/f32540c61fd8c8f585bbb99161632934.jpg"  # noqa: E501
         )
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author.bot:
-            return
-
-        args = message.content.split(" ")
-        if args[0] == "bruh":
-            await message.channel.send(":moyai:")
-        elif (
-            args[0] in ("i'm", "im", "i am") and " ".join(args[1:]) == "horny"
-        ):  # noqa: E501
-            await message.channel.send(
-                "https://tenor.com/view/vorzek-vorzneck-oglg-og-lol-gang-gif-24901093"  # noqa: E501
-            )
-        elif args[0] == "moo":
-            await message.channel.send(
-                "https://tenor.com/view/holy-cow-holy-cow-gif-25938150"
-            )
-
 
 async def setup(bot):  # sourcery skip: instance-method-first-arg-name
     await bot.add_cog(Fun(bot))
